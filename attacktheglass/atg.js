8715ca955b9f0486213217caf88cd6d163cb3567
Austin Dreosch <austindreosch@gmail.com>
2022-06-24 20:04:00 -0500

    hgfhg

---
+++ attacktheglass/atg.js
@@ -0,0 +1,34 @@
+    <div class="divtablerow">
+        <div class="num">
+          <span class="num-inner"></span> //enter number
+        </div>
+        <div class="img">
+          <img class="img-inner"src=""/> //enter url link
+        </div>
+        <div class="name">
+          <span class="name-inner"></span> //enter name
+        </div>
+        <div class="info">
+          <div class="infoinner">
+            <div class="age">
+              <p class="ageinner"></p> //enter age
+            </div>
+            <div class="stats">
+              <div class="">FG%</div> //enter class
+              <div class="">FT%</div> //enter class
+              <div class="">3PM</div> //enter class
+              <div class="">PTS</div> //enter class
+              <div class="">REB</div> //enter class
+              <div class="">AST</div> //enter class
+              <div class="">STL</div> //enter class
+              <div class="">BLK</div> //enter class
+              <div class="">TO</div> //enter class
+            </div>
+          </div>
+        </div>
+        <div class="note">
+          <div class="note-inner"></div> //enter note
+        </div>
+    </div>
+
+
