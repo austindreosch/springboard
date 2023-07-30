9eefdc0e667136a56bf865d76ffd3b0c827b4040
Austin Dreosch <83053022+austindreosch@users.noreply.github.com>
2023-07-29 19:50:48 -0700

    problems

--- misc/average-pair.js
+++ misc/average-pair.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(sortedInts, targetAvg) {
+    let left = 0;
+    let right = sortedInts.length - 1;
+
+    while (left < right){
+        const currPair = sortedInts[left] + sortedInts[right];
+        if (currPair / 2 === targetAvg){
+            return true;
+        } else if (currPair / 2 < targetAvg) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return false;
 }
--- misc/construct-note.js
+++ misc/construct-note.js
@@ -1,3 +1,31 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+
+// constructNote
+// Write a function called constructNote, which accepts two strings, a message and some letters. The function should return true if the message can be built with the letters that you are given; otherwise, it should return false.
+
+// Assume that there are only lowercase letters and no space or special characters in both the message and the letters.
+
+
+// constructNote('aa', 'abc') // false
+// constructNote('abc', 'dcba') // true
+// constructNote('aabbcc', 'bcabcaddff') // true
+
+function constructNote(letters, message) {
+    // loop over letters and make hash map
+    const freqCounter = {}
+    for (let letter of letters){
+        freqCounter[letter] = (freqCounter[letter] + 1) || 1
+    }
+    // then loop over message and decrement based on finding letters
+    for (let char of message){
+        // if u ever see 0 or not in hash map, return false
+        if(freqCounter[char] > 0){
+            freqCounter[char]--;
+        } else{
+            return false
+        }
+    }
+    return true;
 }
+
+constructNote('aa', 'abc')
